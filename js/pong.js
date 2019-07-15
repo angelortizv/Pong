1f0ab0c7579eafc9f187045802b2de0e4b0b7b9e
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-07-14 20:10:07 -0600

    [UPDATE] Game entities was created

--- js/pong.js
+++ js/pong.js
@@ -10,5 +10,31 @@
 var canvas = document.getElementById('game-canvas')
 var ctx = canvas.getContext('2d')
 
+var playerPaddle = {
+    width = 15,
+    height = 90,
+    x: 20,
+    y: 100,
+    score = 0
+};
+
+var computerPaddle = {
+    width: 15,
+    height: 90,
+    x: canvas.width - 35,
+    y: 400,
+    vx: 0,
+    vy: 0.1,    
+    score: 0,
+};
+
+var ball = {
+    width: 15,
+    height: 15,
+    x: 200,
+    y: 50,
+    vx: -10,
+    vy: 3
+};
 
 document.getElementById('game-canvas').style.background = '#000'
