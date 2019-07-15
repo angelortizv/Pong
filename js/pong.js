53320950695dcf92ee752a5b5e8900f116a95807
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-07-14 20:06:31 -0600

    [UPDATE] Create game-canvas

---
+++ js/pong.js
@@ -0,0 +1,14 @@
+/** 
+ * @file pong.js
+ * @version 1.0
+ * @date 14/07/2019
+ * @author angelortizv
+ * @title PONG
+ * @brief Clone of the classic Atari Pong game. See Full Project https://github.com/angelortizv/Pong for details. 
+ */
+
+var canvas = document.getElementById('game-canvas')
+var ctx = canvas.getContext('2d')
+
+
+document.getElementById('game-canvas').style.background = '#000'
